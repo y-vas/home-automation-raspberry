3b297b8242a7c81c628f75ef36bbd54b1149bb40
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2021-01-02 19:16:11 +0100

    add gpio module

--- lights.cpp
+++ lights.cpp
@@ -2,16 +2,37 @@
 #include <string>
 #include <unordered_map>
 
+#include <unistd.h> // sleep for linux
+#include "gpio.cpp"
+
 using namespace std;
 
-unordered_map<int, int> pins;
+unordered_map<int, int> PINS {{
+  { 4, 3 },
+  { 0, 2 },
+  { 2, 2 },
+  { 6, 2 },
+  { 8, 2 },
+  { 1, 1 },
+  { 3, 1 },
+  { 5, 1 },
+  { 7, 1 }
+}};
+
+unordered_map<int, string> PINS_RUTINES {{
+  { 4, "06-22" },
+  { 0, "00-24" },
+}};
+
+
 
 // detects the sound from where it comes  and returns the pin to switch
 int detect_sound(){
 
-
-
-  return 1;
+  // waiting for the sound detectors to test software
+  int pin_detected  = 3;
+  int pin_to_switch = PINS[ pin_detected ];
+  return pin_to_switch;
 }
 
 // switches the light
@@ -20,18 +41,26 @@ int switch_light( int pin ){
   return 1;
 }
 
-int main (){
-  pins[5] = 1;
+int main(){
 
-  for (auto x : pins)
-      cout << x.first << " " << x.second << endl;
+  setup_gpio( 17 , INPUT , PUD_OFF );
+
+  // for (auto x : pins)
+  //     cout << x.first << " " << x.second << endl;
 
   while ( 1 ) {
-    int switch_pin = detect_sound();
+
+    int pin_to_switch = detect_sound();
+
+
+    // usleep( 100000 );
+    // cout << "World" << endl;
+
 
 
 
   }
+
 
   return 0;
 }
