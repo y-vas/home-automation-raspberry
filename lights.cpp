1d8c41067ad82c7219b8f2126edc1d5b74b43ee4
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2021-01-02 11:03:52 +0100

    comments

--- lights.cpp
+++ lights.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
-#include <fstream>
 #include <string>
+#include <unordered_map>
 
-int main() {
+using namespace std;
+
+unordered_map<int, int> pins;
+
+// detects the sound from where it comes  and returns the pin to switch
+int detect_sound(){
 
 
 
-   return 0;
+  return 1;
+}
+
+// switches the light
+int switch_light( int pin ){
+
+  return 1;
+}
+
+int main (){
+  pins[5] = 1;
+
+  for (auto x : pins)
+      cout << x.first << " " << x.second << endl;
+
+  while ( 1 ) {
+    int switch_pin = detect_sound();
+
+
+
+  }
+
+  return 0;
 }
